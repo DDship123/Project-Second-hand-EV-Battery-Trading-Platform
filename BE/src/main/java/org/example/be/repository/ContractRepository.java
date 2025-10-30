61fb8b595a99350ad9cbf7f6be475335accbea17
DESKTOP-J4748V1\LENOVO <bentan5650@gmail.com>
2025-10-30 14:27:34 +0700

    Contracts

--- BE/src/main/java/org/example/be/repository/ContractRepository.java
+++ BE/src/main/java/org/example/be/repository/ContractRepository.java
@@ -4,6 +4,10 @@ import org.example.be.entity.Contract;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.Optional;
+
 @Repository
 public interface ContractRepository extends JpaRepository<Contract, Integer> {
+    Optional<Contract> findByTransaction_TransactionsId(Integer transactionId);
+    boolean existsByTransaction_TransactionsId(Integer transactionId);
 }
