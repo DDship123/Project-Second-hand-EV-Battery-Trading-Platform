b98ec2f178787501212a25e77da0d1691a96279f
DDship123 <96617601+DDship123@users.noreply.github.com>
2025-11-27 19:59:01 +0700

    cập nhật

--- FE/src/main/java/org/example/fe/controller/CommentController.java
+++ FE/src/main/java/org/example/fe/controller/CommentController.java
@@ -1,6 +1,7 @@
 package org.example.fe.controller;
 
 import jakarta.servlet.http.HttpSession;
+import org.example.fe.response.ApiResponse;
 import org.example.fe.response.CommentResponse;
 import org.example.fe.response.MemberResponse;
 import org.example.fe.response.PostResponse;
@@ -10,6 +11,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 @Controller
 public class CommentController {
@@ -19,7 +21,7 @@ public class CommentController {
     @PostMapping("/create/comment")
     public String createComment(@RequestParam("postId") int postId,
                                 @RequestParam("rating") int rating,
-                                @RequestParam("comment") String comment, HttpSession session, Model model) {
+                                @RequestParam("comment") String comment, HttpSession session, Model model, RedirectAttributes redirectAttributes) {
         MemberResponse memberResponse = (MemberResponse) session.getAttribute("user");
 
         CommentResponse commentResponse = new CommentResponse();
@@ -30,7 +32,12 @@ public class CommentController {
         PostResponse postResponse = new PostResponse();
         postResponse.setPostsId(postId);
         commentResponse.setPost(postResponse);
-        commentService.createComment(commentResponse);
+        ApiResponse<CommentResponse> response = commentService.createComment(commentResponse);
+        if (response.getPayload() != null) {
+            redirectAttributes.addAttribute("successMessage", "Gửi bình luận thành công.");
+        } else {
+            redirectAttributes.addAttribute("errorMessage", "Gửi bình luận thất bại. Vui lòng thử lại.");
+        }
         return "redirect:/home/product/detail/"+postId;
     }
 }
